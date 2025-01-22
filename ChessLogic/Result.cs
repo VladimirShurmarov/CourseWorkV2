377b148c10a237fc76fc8a6ee06ca7854159e99d
AAA <shturm2001@mail.ru>
2025-01-22 22:39:19 +0700

    добавлены мат и патовая ситуация

---
+++ ChessLogic/Result.cs
@@ -0,0 +1,23 @@
+﻿namespace ChessLogic
+{
+    public class Result
+    {
+        public Player Winner { get; }
+        public EndReason Reason { get; }
+        public Result(Player winner, EndReason reason) // Определение результата
+        {
+            Winner = winner;
+            Reason = reason;
+        }
+
+        public static Result Win(Player winner) // Описание победы
+        {
+            return new Result(winner, EndReason.Checkmate);
+        }
+
+        public static Result Draw(EndReason reason) // Описание ничьей
+        {
+            return new Result(Player.None, reason);
+        }
+    }
+}
