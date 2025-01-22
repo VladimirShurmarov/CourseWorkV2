f10a9dd86f1e57d2c8d3d6648a653bb703d2f5b1
AAA <shturm2001@mail.ru>
2025-01-22 21:13:33 +0700

    привязка ходов к интерфейсу пользователя, добавление подсветки возможных ходов и отображение принадлежности хода через цвет курсора

---
+++ ChessUI/ChessCursors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ChessUI
+{
+    // Изменение цвета курсора для отображения, кому принадлежит текущий ход
+    public static class ChessCursors
+    {
+        public static readonly Cursor WhiteCursor = LoadCursor("Assets/CursorW.cur");
+        public static readonly Cursor BlackCursor = LoadCursor("Assets/CursorB.cur");
+
+        private static Cursor LoadCursor(string filePath)
+        {
+            Stream stream = Application.GetResourceStream(new Uri(filePath, UriKind.Relative)).Stream;
+            return new Cursor(stream, true);
+        }
+    }
+}
--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -18,8 +18,11 @@ namespace ChessUI
     public partial class MainWindow : Window
     {
         private readonly Image[,] pieceImages = new Image[8, 8];
+        private readonly Rectangle[,] highlights = new Rectangle[8, 8];
+        private readonly Dictionary<Position, Move> moveCache = new Dictionary<Position, Move>();
 
         private GameState gameState;
+        private Position selectedPos = null;
 
         public MainWindow()
         {
@@ -28,9 +31,10 @@ namespace ChessUI
 
             gameState = new GameState(Player.White, Board.Initial());
             DrawBoard(gameState.Board);
+            SetCursor(gameState.CurrentPlayer);
         }
 
-        private void InitializeBoard()
+        private void InitializeBoard() // Создание доски
         {
             for (int r = 0; r < 8; r++)
             {
@@ -39,12 +43,16 @@ namespace ChessUI
                     Image image = new Image();
                     pieceImages[r, c] = image;
                     PieceGrid.Children.Add(image);
+
+                    Rectangle highlight = new Rectangle();
+                    highlights[r, c] = highlight;
+                    HighlightGrid.Children.Add(highlight);
                 }
             }
         }
 
 
-        private void DrawBoard(Board board)
+        private void DrawBoard(Board board) // Загрузка фигур
         {
             for (int r = 0; r < 8; r++)
             {
@@ -53,6 +61,101 @@ namespace ChessUI
                     Piece piece = board[r, c];
                     pieceImages[r,c].Source = Images.GetImage(piece);
                 }
+            }
+        }
+
+        // Обработка нажатия игрока
+        private void BoardGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point point = e.GetPosition(BoardGrid);
+            Position pos = ToSquarePosition(point);
+
+            if (selectedPos == null)
+            {
+                OnFromPositionSelected(pos);
+            }
+            else
+            {
+                OnToPositionSelected(pos);
+            }
+        }
+
+        private Position ToSquarePosition(Point point)
+        {
+            double squareSize = BoardGrid.ActualWidth / 8;
+            int row = (int)(point.Y / squareSize);
+            int col = (int)(point.X / squareSize);
+            return new Position(row, col); 
+        }
+
+        // Обработка первого клика по фигуре (ее взятие)
+        private void OnFromPositionSelected(Position pos)
+        {
+            IEnumerable<Move> moves = gameState.LegalMovesForPiece(pos);
+
+            if (moves.Any()) // Обработка случая, если фигуре некуда двинуться
+            {
+                selectedPos = pos;
+                CacheMoves(moves);
+                ShowHighlights();
+            }
+        }
+
+        // Обработка второго клика (ход фигуры)
+        private void OnToPositionSelected(Position pos)
+        {
+            selectedPos = null;
+            HideHighlights();
+
+            if (moveCache.TryGetValue(pos, out Move move)) // Проверка на возможность хода
+            {
+                HandleMove(move);
+            }
+        }
+
+        private void HandleMove(Move move)
+        {
+            gameState.MakeMove(move);
+            DrawBoard(gameState.Board);
+            SetCursor(gameState.CurrentPlayer);
+        }
+
+        // Обратботка подсветки вариантов ходов у фигуры
+        private void CacheMoves(IEnumerable<Move> moves) // Варианты фигуры
+        {
+            moveCache.Clear();
+
+            foreach (Move move in moves)
+            {
+                moveCache[move.ToPos] = move;
+            }
+        }
+        private void ShowHighlights() // Подсветка
+        {
+            Color color = Color.FromArgb(150, 125, 255, 125);
+
+            foreach (Position to in moveCache.Keys)
+            {
+                highlights[to.Row, to.Column].Fill = new SolidColorBrush(color);
+            }
+        }
+        private void HideHighlights() // Выключение подсветки
+        {
+            foreach (Position to in moveCache.Keys)
+            {
+                highlights[to.Row, to.Column].Fill = Brushes.Transparent;
+            }
+        }
+
+        private void SetCursor(Player player) // Переключение цвета курсора
+        {
+            if (player == Player.White)
+            {
+                Cursor = ChessCursors.WhiteCursor;
+            }
+            else
+            {
+                Cursor = ChessCursors.BlackCursor;
             }
         }
 
